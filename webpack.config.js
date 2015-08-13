b284f234bef62dc07414dc08d7c5099b5b931b2f
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-08-13 17:14:33 +0200

    getting eslint & react together in webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+
+var ROOT_PATH = path.resolve(__dirname);
+var TARGET = process.env.TARGET;
+
+var common = {
+    entry: path.resolve(ROOT_PATH, 'app/main'),
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({
+            title: 'Kanban app'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: path.resolve(ROOT_PATH, 'app')
+            }
+        ]
+    }
+};
+
+
+if (TARGET === 'dev') {
+    module.exports = merge(common, {
+        devtool: 'eval',
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: ['babel?stage=1'],
+                    include: path.resolve(ROOT_PATH, 'app')
+                }
+            ]
+        },
+        devServer: {
+            colors: true,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
